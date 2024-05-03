cc4141075d1ebb4d2b13868fb7b26ee5bbe4532b
mahajanrutu <mahajanrutuja3@gmail.com>
2024-05-03 23:01:49 +0530

    value is equal or not

---
+++ ValueIsEqualOrNot.java
@@ -0,0 +1,20 @@
+class ValueIsEqualOrNot
+{
+  public static void main(String[] args)
+  {
+     int a= Integer.parseInt(args[0]);
+     int b= Integer.parseInt(args[1]);
+      
+    if(a==b)
+{
+ System.out.println("Number is Equal" +(a==b));
+}
+else
+{
+   
+     System.out.println("Number is  Not Equal" +(a!=b));
+}
+
+   
+  }
+}
