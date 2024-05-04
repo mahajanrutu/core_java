7e406b4629198382bf12e8bcda783c13e0f6a6db
mahajanrutu <mahajanrutuja3@gmail.com>
2024-05-04 23:35:42 +0530

    Program to perfom sum of three digit

---
+++ SumOfThreeNum.java
@@ -0,0 +1,15 @@
+class SumOfThreeNum
+{
+   public static void main(String args[])
+   {
+     int num1= Integer.parseInt(args[0]);
+
+     int num2= Integer.parseInt(args[1]);
+
+     int num3= Integer.parseInt(args[2]);
+
+
+
+     System.out.println("Avarage of Three numbers are:" +(num1+num2+num3/3));
+   }
+}
