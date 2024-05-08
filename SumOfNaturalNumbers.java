36df7176624e85db6eff36d44868c420afbce826
mahajanrutu <mahajanrutuja3@gmail.com>
2024-05-08 23:41:03 +0530

    Sum of natural numbers

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,13 @@
+class SumOfNaturalNumbers
+{
+  public static void main(String[] args)
+  {
+    int i, num=100, sum=0;
+
+    for(i=1; i<=num; i++)
+    {
+      sum=sum+i;
+    }
+    System.out.println("Sum Of 100 Natural Numbers are:" +sum);
+  }
+}
