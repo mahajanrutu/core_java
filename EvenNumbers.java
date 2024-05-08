d2f0470e13c3c2a2cecbc9fd1629e47d2a006926
mahajanrutu <mahajanrutuja3@gmail.com>
2024-05-08 23:28:29 +0530

    Finding 1 to 100 Even numbers

---
+++ EvenNumbers.java
@@ -0,0 +1,14 @@
+class EvenNumbers
+{
+  public static void main(String args[])
+  {
+     int i;
+     for(i=1; i<=100; i++)
+      {
+         if(i%2==0)
+        {
+          System.out.println(i);
+        }  
+     }
+  }
+}
