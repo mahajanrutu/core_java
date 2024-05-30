d07ef5b5aebcd090e0a3798a263c4b14c5a5a4cc
mahajanrutu <mahajanrutuja3@gmail.com>
2024-05-30 23:27:43 +0530

    Constructor overloading program

---
+++ pro.java
@@ -0,0 +1,31 @@
+class Addition 
+{
+   public Addition(int x, int y)
+   {	  
+	   super();
+	   System.out.println("Sum of two integer is :"+(x+y));
+   }
+   
+   public Addition(int x, int y, int z)
+   {
+	   this(100,200);
+	   System.out.println("Sum of three integer is :"+(x+y+z));
+   }
+   
+   public Addition(float x, float y)
+   {	
+	   this(10,20,30);
+	   System.out.println("Sum of two float is :"+(x+y));
+   }
+}
+
+public class pro
+ {
+
+	public static void main(String []  args) 
+	{
+		new Addition(2.3f, 7.8F);		
+		
+	}
+
+}
